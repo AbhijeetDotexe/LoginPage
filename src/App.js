0a934db530701330f56bf307dcf1c026bc01fb2b
AbhijeetDotexe <abhijeet4rana@gmail.com>
2024-01-31 16:00:49 +0530

    Made the login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import Validation from './components/Validation';
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 function App() {
   return (
     <div>
-      This is just for testing
+      <Validation/>
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,11 @@ import React from 'react'
 function Login() {
   return (
     <div className='login'>
-        
+        <h1>Login</h1>
+        <input type="email" name='email' placeholder='Enter your emailID' ></input>
+
+        <input type="password" name='password' placeholder='Enter your Password'></input>
+        <button type='submit'>Login</button>
     </div>
   )
 }
